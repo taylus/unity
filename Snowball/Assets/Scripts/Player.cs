e0f349fedf5ab0f985a5efc2a791e7aa46ca4175
Taylus <dragondude@gmail.com>
2016-03-07 22:48:42 -0600

    Rolling snowball game initial commit

---
+++ Snowball/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float MaxSpeed = 1.0f;
+    private Rigidbody2D body;
+
+    public void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        if (body == null) throw new MissingComponentException("Expected attached Rigidbody2D");
+
+        StartCoroutine("Grow");
+    }
+
+    public void FixedUpdate()
+    {
+        float input = Input.GetAxis("Horizontal") * 4;
+        body.AddForce(new Vector2(input, 0));
+        body.velocity = Vector2.ClampMagnitude(body.velocity, MaxSpeed);
+    }
+
+    private IEnumerator Grow()
+    {
+        while (transform.localScale.x <= 1.0f)
+        {
+            transform.localScale += new Vector3(0.001f, 0.001f, 0);
+            body.mass += 0.001f;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
