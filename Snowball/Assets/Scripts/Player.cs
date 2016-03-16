68c8912e833c48f484907db93368a6ce609629a1
Taylus <dragondude@gmail.com>
2016-03-15 20:29:09 -0500

    Making player control snappier

--- Snowball/Assets/Scripts/Player.cs
+++ Snowball/Assets/Scripts/Player.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("Max movement speed in world units/sec.")]
     public float MaxSpeed = 1.0f;
+
     private Rigidbody2D body;
 
     public void Awake()
@@ -16,7 +18,7 @@ public class Player : MonoBehaviour
 
     public void FixedUpdate()
     {
-        float input = Input.GetAxis("Horizontal") * 4;
+        float input = Input.GetAxis("Horizontal") * 8;
         body.AddForce(new Vector2(input, 0));
         body.velocity = Vector2.ClampMagnitude(body.velocity, MaxSpeed);
     }
@@ -26,7 +28,7 @@ public class Player : MonoBehaviour
         while (transform.localScale.x <= 1.0f)
         {
             transform.localScale += new Vector3(0.001f, 0.001f, 0);
-            body.mass += 0.001f;
+            body.mass += 0.005f;
             yield return new WaitForSeconds(0.5f);
         }
     }
