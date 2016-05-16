35888a2e36684259eb467f2370b9f61403a1856c
Taylus <dragondude@gmail.com>
2016-05-15 23:52:05 -0500

    Initial checkin
    With eye tracking

---
+++ RolyPoly/Assets/Scripts/Eye.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eye : MonoBehaviour
+{
+    public bool TrackCursor = true;
+    public GameObject Pupil;
+
+    public void Update()
+    {
+        if (TrackCursor && Pupil != null)
+        {
+            Vector2 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            LookAt(mouseWorldPoint);
+        }
+    }
+
+    private void LookAt(Vector3 position)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, transform.position - position, Mathf.Infinity, 1 << gameObject.layer);
+        Debug.DrawLine(transform.position, position, Color.green);
+        if(hit.collider != null)
+        {
+            Pupil.transform.position = hit.point;
+        }
+        else
+        {
+            Pupil.transform.position = position;
+        }
+    }
+}
