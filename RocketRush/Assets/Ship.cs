568b06dd76822c3c1d34382c0cc7d9c00db209d9
Taylus <dragondude@gmail.com>
2017-09-23 16:44:02 -0500

    Initial commit of a rocket physics game

---
+++ RocketRush/Assets/Ship.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float Thrust;
+    public float Handling; //higher number -> faster turns
+
+    //TODO: add clouds, birds? need some reference points in the sky
+    //TODO: add altimeter
+    //TODO: make fire appear when thrusting
+    //TODO: screen shake when thrusting
+    //TODO: particle/smoke effects when thrusting
+    //TODO: replacable/upgradable ship parts that affect stats
+
+    //other stats?
+    //- top speed
+    //- weight
+    //- fuel capacity
+    //- fuel efficiency
+    //- hull (hp)
+    //- shields?
+
+    public void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        body.rotation += Input.GetAxis("Horizontal") * -Handling;
+        body.AddForce(transform.up * Input.GetAxis("Vertical") * Thrust);
+    }
+}
