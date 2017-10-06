999fc142f06108c0a2ed8da239f106de72820f21
Taylus <dragondude@gmail.com>
2017-10-05 21:41:43 -0500

    Adding a fuel collectable spawner

---
+++ RocketRush/Assets/FuelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Creates <see cref="Fuel"/> prefabs randomly within a space. 
+/// </summary>
+public class FuelManager : MonoBehaviour
+{
+    public Fuel FuelPrefab;
+    public Rect SpawnArea;
+    public float SpawnDistance;
+
+    private void OnDrawGizmosSelected()
+    {
+        //draw the spawn area in the editor window
+        Gizmos.color = new Color(0, 1, 0);
+        Gizmos.DrawWireCube(transform.position + (Vector3)SpawnArea.position, SpawnArea.size);
+    }
+
+    private void Awake()
+    {
+        SpawnFuelCollectables();
+    }
+
+    private void SpawnFuelCollectables()
+    {
+        for (float y = SpawnDistance; y <= SpawnArea.height; y += SpawnDistance)
+        {
+            var spawnedFuel = Instantiate(FuelPrefab, transform);
+            spawnedFuel.name = "Fuel " + y;
+            spawnedFuel.transform.position = new Vector2(Random.Range(-SpawnArea.width / 2, SpawnArea.width / 2), y);
+        }
+    }
+}
